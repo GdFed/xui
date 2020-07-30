5153eec06829acf3f9492a424afa125e16fba0f9
GdFed <602742646@qq.com>
2020-07-30 16:38:36 +0800

    add ask.js

---
+++ ask.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        type: 'confirm',
+        name: 'private',
+        message: 'this resgistery is private?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'this project author?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'this project description?',
+        default: 'demo'
+    },
+]
